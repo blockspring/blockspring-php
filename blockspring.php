02f28000e029f687ca653f68ac3f4885a4fdf567
paul katsen <paul@blockspring.com>
2014-12-08 00:52:55 -0800

    fix orig php errors, add parsedrun

--- blockspring.php
+++ blockspring.php
@@ -1,14 +1,101 @@
 <?php
 
 class Blockspring {
+  public static function parse($input_params, $json_parsed = true) {
+    $request = new BlockspringRequest();
+
+    # try to parse inputs as json
+    if ($json_parsed == true) {
+      $params = $input_params;
+    } else {
+      $params = json_decode($input_params, true);
+    }
+
+    if (!$params) {
+      throw new Exception("STDIN was not valid JSON.");
+    }
+
+    // If inputs json, check if they're an array.
+    if (!self::is_assoc($params)) {
+      throw new Exception("STDIN was valid JSON, but was not key value pairs.");
+    }
+
+    // Check if following blockspring spec
+    if (isset($params["_blockspring_spec"]) && $params["_blockspring_spec"]) {
+      // We're following spec so lets remove _blockspring_spec, print errors to stderr, and parse files.
+      foreach($params as $key => $val) {
+        if ($key == "_blockspring_spec") {
+          // Remove _blockspring_spec flag from params.
+          unset($params[$key]);
+        } elseif ($key == "_errors" && is_array($params["_errors"])) {
+          // Add errors to request object.
+
+          foreach ($params["_errors"] as $error) {
+            // Make sure the error has a title.
+            if (self::is_assoc($error) && $error["title"]) {
+              $request->addError($error);
+            }
+          }
+        } elseif ($key == "_headers" && is_array($params["_headers"])) {
+          // Add headers to request object.
+          $request->addHeaders($params["_headers"]);
+        } elseif (self::is_assoc($params[$key]) && $params[$key]["filename"]) {
+
+          if ($params[$key]["data"] || $params[$key]["url"]) {
+            // Create temp file
+            $prefix = "-" . $params[$key]["filename"];
+            $tmp_file_name = tempnam(sys_get_temp_dir(), "") . $prefix;
+            $request->params[$key] = $tmp_file_name;
+            $handle = fopen($tmp_file_name, "w");
+
+            // Check if we have raw data
+            if ($params[$key]["data"]) {
+              // Try to decode base64, if not set naively.
+              try {
+                $file_contents = base64_decode($params[$key]["data"]);
+              } catch (Exception $e) {
+                $file_contents = $params[$key]["data"];
+              }
+            } elseif ($params[$key]["url"]) {
+              // Download file and save to tmp file.
+              $opts = array(
+                'http' => array(
+                  'method' => "GET"
+                )
+              );
+
+              $context = stream_context_create($opts);
+              $file_contents = file_get_contents($params[$key]["url"], false, $context);
+            }
+
+            // Write to tmp file
+            fwrite($handle, $file_contents);
+            fclose($handle);
+          } else {
+            // Set naively since no data or url given.
+            $request->params[$key] = $params[$key];
+          }
+        } else {
+          // Handle everything else
+          $request->params[$key] = $params[$key];
+        }
+      }
+    } else {
+      // Not following spec, naively set params.
+      $request->params = $params;
+    }
+
+    return $request;
+  }
+
   public static function run($block, $data = array(), $api_key = null) {
     $api_key = $api_key ? $api_key : getenv('BLOCKSPRING_API_KEY');
 
     // Data must be given as an array, or array of arrays (so it can be json_encoded).
-    if (is_array($data)) {
+    if (self::is_assoc($data)) {
       $json_data = json_encode($data);
     } else {
-      return "Error - the data provided was not an array or nested array: " . print_r($data, true);
+      throw new Exception("your data needs to be a associative array.");
     }
 
     $api_key_string = $api_key ? "api_key=" . $api_key : '';
@@ -24,122 +111,96 @@ class Blockspring {
         'header'  => "Content-type: application/json",
         'method'  => 'POST',
         'content' => $json_data,
+        'ignore_errors' => true
+      ),
+    );
+    $context = stream_context_create($options);
+
+    $result = file_get_contents($url, false, $context);
+
+    try {
+      $blockspring_body = json_decode($result, true);
+    } catch (Exception $e){}
+
+    if (isset($blockspring_body) == true ){
+      return $blockspring_body;
+    } else {
+      return($result);
+    }
+  }
+
+  public static function runParsed($block, $data = array(), $api_key = null) {
+    $api_key = $api_key ? $api_key : getenv('BLOCKSPRING_API_KEY');
+
+    // Data must be given as an array, or array of arrays (so it can be json_encoded).
+    if (self::is_assoc($data)) {
+      $json_data = json_encode($data);
+    } else {
+      throw new Exception("your data needs to be a associative array.");
+    }
+
+    $api_key_string = $api_key ? "api_key=" . $api_key : '';
+
+    $block_parts = explode("/", $block);
+    $block = end($block_parts);
+
+    $blockspring_url = getenv('BLOCKSPRING_URL') ? getenv('BLOCKSPRING_URL') : 'https://sender.blockspring.com';
+
+    $url = "{$blockspring_url}/api_v2/blocks/{$block}?{$api_key_string}";
+    $options = array(
+      'http' => array(
+        'header'  => "Content-type: application/json",
+        'method'  => 'POST',
+        'content' => $json_data,
+        'ignore_errors' => true
       ),
     );
     $context = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
 
     try {
-      $body = json_decode($result);
+      $blockspring_parsed_results = json_decode($result, true);
+      if (self::is_assoc($blockspring_parsed_results)) {
+        $blockspring_parsed_results["_headers"] = self::http_parse_headers(implode("\n",$http_response_header));
+      } else {
+        $blockspring_non_list_json = true;
+      }
     } catch (Exception $e){
-      $body = $result;
     }
 
-    return $body;
+    if (isset($blockspring_parsed_results) == true){
+      if (isset($blockspring_non_list_json)) {
+        return $blockspring_parsed_results;
+      } else {
+        return self::parse($blockspring_parsed_results, true);
+      }
+    } else {
+      return($result);
+    }
   }
 
   public static function define($my_function = null){
-    $request = new BlockspringRequest();
+    $request = self::processArgs(self::processStdin());
     $response = new BlockspringResponse();
 
-    print_r($my_function($request, $response));
-  }
-}
-
-class BlockspringRequest {
-  public $params = array();
-  public $_errors = array();
-
-  public function __construct(){
-    $this->setParamsFromSTDIN();
-    $this->setParamsFromArgs();
+    $my_function($request, $response);
   }
 
-  private function setParamsFromSTDIN() {
+  private function processStdin() {
     // Check if something coming into STDIN.
     if (!posix_isatty(STDIN)) {
       $stdin = '';
       while (false !== ($line = fgets(STDIN))) {
         $stdin .= $line;
       }
-
-      // Try to parse inputs as JSON.
-      $stdin_params = json_decode($stdin, true);
-
-      if (!$stdin_params) {
-        trigger_error("STDIN was not valid JSON.", E_USER_ERROR);
-      }
-
-      // If inputs json, check if they're an array.
-      if (!is_array($stdin_params)) {
-        trigger_error("STDIN was valid JSON, but was not key value pairs.", E_USER_ERROR);
-      }
-
-      // Check if following blockspring spec
-      if (isset($stdin_params["_blockspring_spec"]) && $stdin_params["_blockspring_spec"]) {
-        // We're following spec so lets remove _blockspring_spec, print errors to stderr, and parse files.
-        foreach($stdin_params as $key => $val) {
-          if ($key == "_blockspring_spec") {
-            // Remove _blockspring_spec flag from params.
-            unset($stdin_params[$key]);
-          } elseif ($key == "_errors" && is_array($stdin_params["_errors"])) {
-            // Add errors to request object.
-
-            foreach ($stdin_params["_errors"] as $error) {
-              // Make sure the error has a title.
-              if (is_array($error) && $error["title"]) {
-                $this->addError($error);
-              }
-            }
-          } elseif (is_array($stdin_params[$key]) && $stdin_params[$key]["filename"]) {
-
-            if ($stdin_params[$key]["data"] || $stdin_params[$key]["url"]) {
-              // Create temp file
-              $prefix = $stdin_params[$key]["filename"] . "-";
-              $tmp_file_name = tempnam(sys_get_temp_dir(), $prefix);
-              $this->params[$key] = $tmp_file_name;
-              $handle = fopen($tmp_file_name, "w");
-
-              // Check if we have raw data
-              if ($stdin_params[$key]["data"]) {
-                // Try to decode base64, if not set naively.
-                try {
-                  $file_contents = base64_decode($stdin_params[$key]["data"]);
-                } catch (Exception $e) {
-                  $file_contents = $stdin_params[$key]["data"];
-                }
-              } elseif ($stdin_params[$key]["url"]) {
-                // Download file and save to tmp file.
-                $opts = array(
-                  'http' => array(
-                    'method' => "GET"
-                  )
-                );
-
-                $context = stream_context_create($opts);
-                $file_contents = file_get_contents($stdin_params[$key]["url"], false, $context);
-              }
-
-              // Write to tmp file
-              fwrite($handle, $file_contents);
-              fclose($handle);
-            } else {
-              // Set naively since no data or url given.
-              $this->params[$key] = $stdin_params[$key];
-            }
-          } else {
-            // Handle everything else
-            $this->params[$key] = $stdin_params[$key];
-          }
-        }
-      } else {
-        // Not following spec, naively set params.
-        $this->params = $stdin_params;
-      }
+      $request = self::parse($stdin, false);
+      return $request;
+    } else {
+      return new BlockspringRequest();
     }
   }
 
-  private function setParamsFromArgs() {
+  private function processArgs($request) {
     global $argv;
 
     $sys_args = array();
@@ -152,9 +213,69 @@ class BlockspringRequest {
     }
 
     foreach ($sys_args as $key => $val) {
-      $this->params[$key] = $val;
+      $request->params[$key] = $val;
+    }
+
+    return $request;
+  }
+
+  private function is_assoc($array) {
+    if (is_array($array)){
+      if (empty($array)){
+        return true;
+      } else {
+        $keys = array_keys($array);
+        return array_keys($keys) !== $keys;
+      }
+    } else {
+      return false;
     }
   }
+
+  private function http_parse_headers($raw_headers) {
+   $headers = array();
+   $key = ''; // [+]
+
+   foreach(explode("\n", $raw_headers) as $i => $h)
+   {
+       $h = explode(':', $h, 2);
+
+       if (isset($h[1]))
+       {
+           if (!isset($headers[$h[0]]))
+               $headers[$h[0]] = trim($h[1]);
+           elseif (is_array($headers[$h[0]]))
+           {
+               // $tmp = array_merge($headers[$h[0]], array(trim($h[1]))); // [-]
+               // $headers[$h[0]] = $tmp; // [-]
+               $headers[$h[0]] = array_merge($headers[$h[0]], array(trim($h[1]))); // [+]
+           }
+           else
+           {
+               // $tmp = array_merge(array($headers[$h[0]]), array(trim($h[1]))); // [-]
+               // $headers[$h[0]] = $tmp; // [-]
+               $headers[$h[0]] = array_merge(array($headers[$h[0]]), array(trim($h[1]))); // [+]
+           }
+
+           $key = $h[0]; // [+]
+       }
+       else // [+]
+       { // [+]
+           if (substr($h[0], 0, 1) == "\t") // [+]
+               $headers[$key] .= "\r\n\t".trim($h[0]); // [+]
+           elseif (!$key) // [+]
+               $headers[0] = trim($h[0]);trim($h[0]); // [+]
+       } // [+]
+   }
+
+   return $headers;
+  }
+}
+
+class BlockspringRequest {
+  public $params = array();
+  public $_errors = array();
+  public $_headers = array();
 
   public function getErrors(){
     return $this->_errors;
@@ -162,6 +283,14 @@ class BlockspringRequest {
 
   public function addError($error){
     array_push($this->_errors, $error);
+  }
+
+  public function getHeaders(){
+    return $this->_headers;
+  }
+
+  public function addHeaders($headers){
+    $this->_headers = $headers;
   }
 }
 
